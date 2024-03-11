b3fad6ea4e0a219aa19a0dfc9cd838cfc5137138
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-03-10 21:43:51 -0300

    feat: add Routes frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Clientes from "./pages/Clientes";
+import Vendas from "./pages/Vendas";
+import Produtos from "./pages/Produtos";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          {/* As rotas so vao existir aqui */}
+          <Route path="/" element={<Clientes />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/vendas" element={<Vendas />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
